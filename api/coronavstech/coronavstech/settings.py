9a74d3a99a8283bc3962aebeabdea46e69a89228
Eden Marco <emarco177@gmail.com>
2020-10-31 12:29:14 +0200

    added email mechanism

--- api/coronavstech/coronavstech/settings.py
+++ api/coronavstech/coronavstech/settings.py
@@ -120,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "israeltechlayoffs@gmail.com"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_PASSWORD = "!ADS213FGBC!!"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
