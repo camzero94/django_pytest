3057edfda2ab4496469e75d00dd2e5d397c1c64b
Eden Marco <emarco177@gmail.com>
2020-11-07 15:36:50 +0200

    fibonacci

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -1,18 +1,9 @@
-from typing import Callable
-
 import pytest
 
 from fibonacci.naive import fibonacci_naive
 
 
-@pytest.mark.parametrize(
-    "fib_func",
-    [
-        fibonacci_naive,
-    ],
-)
-@pytest.mark.parametrize("n,expected", [(5, 5)])
-def test_naive( fib_func: Callable[[int], int], n: int, expected: int
-) -> None:
-    res = fib_func(n)
+@pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_naive(n: int, expected: int) -> None:
+    res = fibonacci_naive(n=n)
     assert res == expected
