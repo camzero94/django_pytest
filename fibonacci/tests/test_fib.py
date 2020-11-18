50d8fc466d6a427264954680401852fb49a950fb
Eden Marco <emarco177@gmail.com>
2020-11-18 19:53:53 +0200

    added dynamic programming implementation

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -1,7 +1,7 @@
 from typing import Callable
 
 import pytest
-
+from fibonacci.dynamic import fibonacci_dynamic, fibonacci_dynamic_v2
 from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
 from fibonacci.naive import fibonacci_naive
 from fixtures import time_tracker
@@ -9,7 +9,14 @@ from fixtures import time_tracker
 
 @pytest.mark.parametrize("n,expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
 @pytest.mark.parametrize(
-    "fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached]
+    "fib_func",
+    [
+        fibonacci_naive,
+        fibonacci_cached,
+        fibonacci_lru_cached,
+        fibonacci_dynamic,
+        fibonacci_dynamic_v2,
+    ],
 )
 def test_fibonacci(
     time_tracker, fib_func: Callable[[int], int], n: int, expected: int
