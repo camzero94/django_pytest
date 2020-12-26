cf87c8c0bf142d4e6bcb6c20f8c9b8f1cf82ca38
Eden Marco <emarco177@gmail.com>
2020-12-26 10:26:12 +0200

    added test with exception handling

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -85,6 +85,16 @@ def test_should_be_skipped() -> None:
     assert 1 == 2
 
 
+def raise_covid19_exception() -> None:
+    raise ValueError("CoronaVirus Exception")
+
+
+def test_raise_covid19_exception_should_pass() -> None:
+    with pytest.raises(ValueError) as e:
+        raise_covid19_exception()
+    assert "CoronaVirus Exception" == str(e.value)
+
+
 # --------------Learn about fixtures tests--------------
 
 
