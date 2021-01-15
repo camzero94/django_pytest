6143c3edeb6e8f3b3dfc292a46e3f66c0005afe4
Eden Marco <emarco177@gmail.com>
2021-01-15 16:28:01 +0200

    added tests that assert logs

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -95,6 +95,29 @@ def test_raise_covid19_exception_should_pass() -> None:
     assert "CoronaVirus Exception" == str(e.value)
 
 
+import logging
+
+logger = logging.getLogger("CORONA_LOGS")
+
+
+def function_that_logs_something() -> None:
+    try:
+        raise ValueError("CoronaVirus Exception")
+    except ValueError as e:
+        logger.warning(f"I am logging {str(e)}")
+
+
+def test_logged_warning_level(caplog) -> None:
+    function_that_logs_something()
+    assert "I am logging CoronaVirus Exception" in caplog.text
+
+
+def test_logged_info_level(caplog) -> None:
+    with caplog.at_level(logging.INFO):
+        logger.info("I am logging info level")
+        assert "I am logging info level" in caplog.text
+
+
 # --------------Learn about fixtures tests--------------
 
 
