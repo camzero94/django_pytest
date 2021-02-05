9becd0b52c7dc63ca1311402d664262387707ebd
Eden Marco <emarco177@gmail.com>
2021-02-05 11:26:36 +0200

    added mock responses

--- api/coronavstech/companies/tests/test_generic_api.py
+++ api/coronavstech/companies/tests/test_generic_api.py
@@ -31,3 +31,50 @@ def test_create_company_with_layoffs_django_agnostic() -> None:
 def cleanup_company(company_id: str) -> None:
     response = requests.delete(url=f"http://127.0.0.1:8000/companies/{company_id}")
     assert response.status_code == 204
+
+
+@pytest.mark.crypto
+def test_dogecoin_api() -> None:
+    response = requests.get(
+        url="https://api.cryptonator.com/api/ticker/doge-usd",
+        headers={"User-Agent": "Mozilla/5.0"},
+    )
+
+    assert response.status_code == 200
+    response_content = json.loads(response.content)
+    assert response_content["ticker"]["base"] == "DOGE"
+    assert response_content["ticker"]["target"] == "USD"
+
+
+import responses
+
+
+@pytest.mark.crypto
+@responses.activate
+def test_mocked_dogecoin_api() -> None:
+    responses.add(
+        method=responses.GET,
+        url="https://api.cryptonator.com/api/ticker/doge-usd",
+        json={
+            "ticker": {
+                "base": "EDEN",
+                "target": "EDEN-USD",
+                "price": "0.04535907",
+                "volume": "4975940509.75870037",
+                "change": "-0.00052372",
+            },
+            "timestamp": 1612515303,
+            "success": True,
+            "error": "",
+        },
+        status=200,
+    )
+    response = requests.get(
+        url="https://api.cryptonator.com/api/ticker/doge-usd",
+        headers={"User-Agent": "Mozilla/5.0"},
+    )
+
+    assert response.status_code == 200
+    response_content = json.loads(response.content)
+    assert response_content["ticker"]["base"] == "EDEN"
+    assert response_content["ticker"]["target"] == "EDEN-USD"
